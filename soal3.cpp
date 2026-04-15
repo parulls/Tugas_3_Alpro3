ba5a61b790746fd4d1fe91f40195b1fc8857c98b
amirahlarasatis-cell <amirahlarasatis@gmail.com>
2026-04-15 12:05:28 +0700

    Implement leap year checker in soal3.cpp
    Add functionality to check leap years for a given range.

--- soal3.cpp
+++ soal3.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+using namespace std;
 
+bool isKabisat(int tahun) {
+    if((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
+        return true; 
+    }
+    return false; 
+}
+
+int main() {
+    int A, B;
+    
+    cout << "Masukkan tahun (A): ";
+    cin >> A;
+    cout << "Masukkan interval (B): ";
+    cin >> B;
+    
+    cout << endl;
+    
+    if(B >= 0) {
+
+        for(int i = 0; i <= B; i++) { 
+            int tahun = A + i;
+            if(isKabisat(tahun)) {
+                cout << tahun << " kabisat" << endl;
+            } else {
+                cout << tahun << " bukan kabisat" << endl;
+            }
+        }
+    } else {
+
+        for(int i = 0; i >= B; i--) {
+            int tahun = A + i;
+            if(isKabisat(tahun)) {
+                cout << tahun << " kabisat" << endl;
+            } else {
+                cout << tahun << " bukan kabisat" << endl;
+            }
+        }
+    }
+return 0;
+}
