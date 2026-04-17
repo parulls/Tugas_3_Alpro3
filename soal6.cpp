1c6ad46a987c5b508fd541f92fcdcaced5afb819
amirahlarasatis-cell <amirahlarasatis@gmail.com>
2026-04-17 09:36:12 +0700

    Add program to calculate sum of squares of even numbers

--- soal6.cpp
+++ soal6.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    int jumlah = 0;
+
+    cout << "Masukkan nilai n: ";
+    cin >> n;
+
+    for (int i = 2; i <= n; i += 2) {
+        jumlah += i * i;
+    }
+
+    cout << "Hasil: " << jumlah << endl;
+
+    return 0;
+}
