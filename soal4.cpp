b3f8eeb4f37aeb85c9502f4bcbb14b81008e4556
amirahlarasatis-cell <amirahlarasatis@gmail.com>
2026-04-17 09:34:57 +0700

    Implement supporter happiness level program

--- soal4.cpp
+++ soal4.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout<<"===PROGRAM TINGKAT KESENANGAN SUPORTER===\n";
+    cout<<"Masukan Tingkat Kesenangan anda(1-10000): ";
+    cin >> n;
+    if(n<=10000){
+    cout << "H";
+
+    for (int i = 0; i < n; i++) {
+        cout << "o";
+    }
+
+    cout << "r";
+
+    for (int i = 0; i < n; i++) {
+        cout << "e";
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << "!";
+    }
+
+    cout << endl;
+}
+    else{
+        cout<<"Anda Melampaui batas kesenangan"<<endl;
+    }
+
+    return 0;
+}
